a4d8e7b3c4bf56c6779b388233e772a2cd4f2bd2
podkova <podkovax@gmail.com>
2021-02-06 23:40:31 +0100

    * comment author and date are moved to the top of the comment and pimped up visually * minor cleanup

--- src/axo.js
+++ src/axo.js
@@ -6,31 +6,47 @@ function linkify(text) {
     });
 }
 
-var targetNode = document.getElementById('gridHeader');
-
-var config = { childList: true, subtree: true };
-
 var callback = function(mutationsList, observer) {
     for(var mutation of mutationsList) {
         if (mutation.type == 'childList') {
             for (var node of mutation.addedNodes)
             {
+                // DESCRIPTION
                 if (node.className == "item-field-table")
                 {
                     var jnode = $(node);
                     jnode.find("[class='field']").each(function(index){
                         if ($(this).find('div').length == 0)
                         {
+                            // Convert dead links to hyperlinks
                             var txt = $(this).html();
                             txt = linkify(txt);
                             $(this).html(txt);
                         }
                     });
                 }
+                // COMMENTS
                 else if (node.tagName == "UL")
                 {
                     var jnode = $(node);
                     jnode.find(".comment-text").each(function(index){
+
+                        // Move comment author's name to the left and make it more visible
+                        // - bold
+                        // - bigger
+                        // - not italic
+                        // - without length limit
+                        var meta = $(this).next();
+                        meta.css({ 'text-align' : 'left', 'padding' : '3px', 'margin' : '0', 'background' : '#d7e0e0' });
+                        meta.find('.comment-author').css({ 'font-weight' : 'bold', 'font-size' : '12px', 'font-style' : 'normal', 'max-width' : 'inherit', 'vertical-align' : 'sub' });
+
+                        // Move comment's text BELOW the comment's author
+                        $(this).insertAfter(meta);
+
+                        // Indent the comment's text slightly to differentiate it from the author's name
+                        $(this).css('padding', '4px 10px 4px 0px');
+
+                        // Convert dead links to hyperlinks
                         var txt = $(this).html();
                         txt = linkify(txt);
                         $(this).html(txt);
@@ -41,5 +57,7 @@ var callback = function(mutationsList, observer) {
     }
 };
 
+var targetNode = document.getElementById('gridHeader');
+var config = { childList: true, subtree: true };
 var observer = new MutationObserver(callback);
 observer.observe(targetNode, config);
