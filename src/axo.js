35d091ea2008cb3988b411ab21ec9195c3bd5c04
podkova <podkovax@gmail.com>
2021-02-07 19:47:35 +0100

    * Better handling of the Description field in the issue view * Handled comments and description in side panels

--- src/axo.js
+++ src/axo.js
@@ -1,35 +1,59 @@
 // https://stackoverflow.com/a/8943487
 let URL_REGEX =/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
-function linkify(text) {
+function linkifyText(text) {
     return text.replace(URL_REGEX, function(url) {
         return '<a href="' + url + '">' + url + '</a>';
     });
 }
 
-var callback = function(mutationsList, observer) {
+function linkifyDOMElement(domElem) {
+    var jElem = $(domElem);
+    var txt = jElem.html();
+    txt = linkifyText(txt);
+    jElem.html(txt);
+}
+
+let sidePanelDescCallback = function(mutationsList, observer) {
+    // Description - side panel
+    for (var mutation of mutationsList) {
+        if (mutation.target.className == "content")
+            linkifyDOMElement(mutation.target);
+    }
+}
+
+let mainCallback = function(mutationsList, sidePanelDescObserver) {
     for(var mutation of mutationsList) {
         if (mutation.type == 'childList') {
             for (var node of mutation.addedNodes)
             {
-                // DESCRIPTION
+                // Description - issue view
                 if (node.className == "item-field-table")
                 {
                     var jnode = $(node);
                     jnode.find("[class='field']").each(function(index){
-                        if ($(this).find('div').length == 0)
+                        if ($(this).parent().prev().children().first().text() == "Description:") // Beware, web crawling voodoo
                         {
                             // Convert dead links to hyperlinks
                             var txt = $(this).html();
-                            txt = linkify(txt);
+                            txt = linkifyText(txt);
                             $(this).html(txt);
+
+                            return false; // break each()
                         }
                     });
                 }
-                // COMMENTS
+                // Description - side panel
+                else if (node.id == "descriptionAccordionItem")
+                {
+                    // Install the side panel desc observer
+                    var sidePanelDescConfig = { characterData: true, attributes: true, subtree: true };
+                    var sidePanelDescObserver = new MutationObserver(sidePanelDescCallback);
+                    sidePanelDescObserver.observe(node, sidePanelDescConfig);
+                }
+                // Comments - everywhere
                 else if (node.tagName == "UL")
                 {
-                    var jnode = $(node);
-                    jnode.find(".comment-text").each(function(index){
+                    $(node).find(".comment-text").each(function(index){
 
                         // Move comment author's name to the left and make it more visible
                         // - bold
@@ -47,9 +71,7 @@ var callback = function(mutationsList, observer) {
                         $(this).css('padding', '4px 10px 4px 0px');
 
                         // Convert dead links to hyperlinks
-                        var txt = $(this).html();
-                        txt = linkify(txt);
-                        $(this).html(txt);
+                        linkifyDOMElement(this);
                     });
                 }
             }
@@ -57,11 +79,12 @@ var callback = function(mutationsList, observer) {
     }
 };
 
-var targetNode = document.getElementById('gridHeader');
-var config = { childList: true, subtree: true };
-var observer = new MutationObserver(callback);
-observer.observe(targetNode, config);
+// Install the main observer
+let mainConfig = { childList: true, subtree: true, characterData: true, attributes: true};
+let mainObserver = new MutationObserver(mainCallback);
+let mainNode = document.getElementById('bottomLayout');
+mainObserver.observe(mainNode, mainConfig);
 
 // Fix style for links (prevents bolding and double underscore)
-var styleTag = $('<style>.comment-text a { border-bottom: 0 !important; font-weight: inherit !important; }</style>')
+let styleTag = $('<style>.comment-text a { border-bottom: 0 !important; font-weight: inherit !important; }</style>')
 $('html > head').append(styleTag);
