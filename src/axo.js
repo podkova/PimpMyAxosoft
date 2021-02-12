33e2fd98a0ff0267b995324ee166c1a4bfee1775
podkova <podkovax@gmail.com>
2021-02-12 01:31:47 +0100

    Further fiddling with the advanced comment section * selected types of events are now gathered from the history panel and injected into the comments section on demand * injections look quite nice now * comments and events are properly sorted

--- src/axo.js
+++ src/axo.js
@@ -15,17 +15,17 @@ function linkifyDOMElement(domElem) {
 
 var gCommentsSection = null;
 
-function addUberViewEvent(afterElem, author, date, text) {
+function addUberViewEvent(afterElem, author, date, timestamp, content) {
 
-    const newElem = $('<li class="axo-lightgrid-item selectable" id="aui-3-18-0-4_39657">' +
+    const newElem = $('<li style="margin-left: 25px" class="axo-lightgrid-item selectable" data-timestamp="' + timestamp + '">' +
     '    <div class="comment-body htmlreset">' +
     '        <div class="comment-container">' +
     '            <div class="comment-content">' +
     '                <div class="comment-meta" style="text-align: left; padding: 3px; margin: 0px; background: rgb(215, 224, 224);">' +
     '                    <span class="comment-author" style="font-weight: bold; font-size: 12px; font-style: normal; max-width: inherit; vertical-align: sub;">' + author + '</span>' +
-    '                    <span class="comment-time">' + date + '</span>' +
+    '                    <span class="comment-time" style="float: right; margin-right: 10px">' + date + '</span>' +
     '                </div>' +
-    '                <div class="comment-text" style="padding: 4px 10px 4px 0px;">' + text + '</div>' +
+    '                <div class="comment-text" style="padding: 4px 10px 4px 0px;">' + content + '</div>' +
     '            </div>' +
     '        </div>' +
     '    </div>' +
@@ -34,11 +34,89 @@ function addUberViewEvent(afterElem, author, date, text) {
     newElem.insertAfter(afterElem);
 }
 
+function addUberViewSimpleEvent(afterElem, author, date, timestamp, content) {
+
+    const newElem = $('<li class="axo-lightgrid-item selectable" data-timestamp="' + timestamp + '">' +
+    '    <div class="comment-body htmlreset">' +
+    '        <div class="comment-container">' +
+    '            <div class="comment-content">' +
+    '                <div class="comment-meta" style="text-align: left; padding: 3px; margin: 0px; ">' +
+    '                    <span class="comment-author" style="font-size: 11px; font-style: normal; max-width: inherit; vertical-align: sub;">' +
+    '                        <b>' + author + '</b> ' + content +
+    '                    </span>' +
+    '                    <span class="comment-time" style="float: right; margin-right: 5px; font-size: 10px; font-style: italic">' + date + '</span>' +
+    '                </div>' +
+    '            </div>' +
+    '        </div>' +
+    '    </div>' +
+    '</li>');
+
+
+    newElem.insertAfter(afterElem);
+}
+
 function refreshAdvancedComments() {
     const historyList = $('.yui3-historyaccordionui-content').find('li.selectable');
+
+    historyList.each(function(){
+        $(this).find('.fa-plus-square-o')[0].click();
+    });
+
+    setTimeout(() => { refreshAdvancedCommentsDelayed(); }, 4000);
+}
+
+function refreshAdvancedCommentsDelayed() {
     const commentList = $(gCommentsSection).find('li.selectable');
 
-    addUberViewEvent(commentList.first(), 'John Silverfoot', '08/12/2020 12:01', 'We have ' + historyList.length + ' history items');
+    commentList.each(function(){
+        const dateStr = $(this).find('.comment-time').first().text();
+        const dateData = Date.parse(dateStr.substring(3, 6) + dateStr.substring(0, 3) + dateStr.substring(6, 100)).toString();
+        $(this).attr('data-timestamp', dateData);
+    });
+
+    const historyList = $('.yui3-historyaccordionui-content').find('li.selectable');
+
+    historyList.each(function(){
+        const dateStr = $(this).find('.body').first().text().replace('on ', '').replace(' at', '');
+        const dateData = Date.parse(dateStr.substring(3, 6) + dateStr.substring(0, 3) + dateStr.substring(6, 100)).toString();
+
+        const author = $(this).find('.header').first().text().replace('Added by: ', '').replace('Changed by: ', '');
+
+        const allowedRowTypes = ['Workflow Step', 'Assigned To', 'Sprint', 'Severity'];
+        let content = '';
+
+        const table = $(this).find('table.changetable').find('tbody');
+        table.find('tr').each(function(){
+            if (allowedRowTypes.includes($(this).children().first().text())) {
+                if (content.length != 0)
+                    content += ' and ';
+
+                switch ($(this).children().eq(0).text()) {
+                    case 'Workflow Step': content += 'set workflow step to'; break;
+                    case 'Assigned To': content += 'assigned issue to'; break;
+                    case 'Sprint': content += 'set sprint to'; break;
+                    case 'Severity': content += 'marked issue as'; break;
+                }
+
+                content += ' <b>' + $(this).children().last().text() + '</b>';
+            }
+        });
+        if (content.length != 0)
+            addUberViewSimpleEvent(commentList.first(), author, dateStr, dateData, content);
+
+        // if (content.length != 0) {
+        //     content = '<span style="font-size: 12px">' + content + '</span>';
+        //     addUberViewEvent(commentList.first(), author, dateStr, dateData, content);                
+        // }
+    });
+
+    const newCommentList = $(gCommentsSection).find('li.selectable');
+
+    const parent = newCommentList.first().parent();
+    newCommentList.sort(function(a,b){
+        const ret = $(a).data('timestamp') < $(b).data('timestamp') ? -1 : 1;
+        return ret;
+    }).appendTo(parent);
 }
 
 let sidePanelDescCallback = function(mutationsList, observer) {
@@ -82,7 +160,6 @@ let mainCallback = function(mutationsList, sidePanelDescObserver) {
                             $(this).parent().prev().children().first().html(
                                 "Comments: <a " +
                                 "style=\"float: right; font-size: 9px; margin-top: -10px\" " +
-                                // "onclick=\"refreshAdvancedComments()\" "+
                                 "id=\"adv-comments-button\" " +
                                 "class=\"button button--basic button--small axo-menuitem-content\">" +
                                 "Advanced</a>");
@@ -116,6 +193,7 @@ let mainCallback = function(mutationsList, sidePanelDescObserver) {
                         var meta = $(this).next();
                         meta.css({ 'text-align' : 'left', 'padding' : '3px', 'margin' : '0', 'background' : '#d7e0e0' });
                         meta.find('.comment-author').css({ 'font-weight' : 'bold', 'font-size' : '12px', 'font-style' : 'normal', 'max-width' : 'inherit', 'vertical-align' : 'sub' });
+                        meta.find('.comment-time').css({ 'float' : 'right', 'margin-right' : '10px' });
 
                         // Move comment's text BELOW the comment's author
                         $(this).insertAfter(meta);
